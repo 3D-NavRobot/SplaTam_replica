55fda617a89601ca8597c174b161987568f9749e
galane-basha <lilinamoa@gmail.com>
2025-04-25 07:09:52 -0400

    Greedy model soup

--- run_batch_splatam.sh
+++ run_batch_splatam.sh
@@ -4,8 +4,8 @@
 #SBATCH --gres=gpu:v100-32:1
 #SBATCH -t 24:00:00
 #SBATCH --mem=32GB
-#SBATCH -o adaptive_lightGlue_splatam_%j.out
-#SBATCH -e adaptive_lightGlue_splatam_%j.err
+#SBATCH -o adaptive_model_soup_splatam_%j.out
+#SBATCH -e adaptive_model_soup_splatam_%j.err
 
 # Load any necessary modules
 module load cuda/12.4.0  
@@ -17,6 +17,6 @@ conda activate /jet/home/gnamomsa/.conda/envs/galane_splatam
 nvidia-smi
 # Run your SplaTAM script
 
-# python scripts/splatam1_soup.py configs/replica/splatam.py
-python scripts/splatam1.py configs/replica/splatam.py
+python scripts/splatam1_soup.py configs/replica/splatam.py
+# python scripts/splatam1.py configs/replica/splatam.py
 # python scripts/splatam_dynamic_lr.py configs/replica/splatam.py
